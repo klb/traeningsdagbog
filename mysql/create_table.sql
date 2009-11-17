3ded3365982878ecb0ee4cd84e9f4c24dac1b0ad
klb <klaus@klausberntsen.dk>
2009-11-17 22:50:11 +0100

    fixed typo

--- mysql/create_table.sql
+++ mysql/create_table.sql
@@ -1,1 +1,22 @@
-# Creating tables
+CREATE DATABASE `TrainingDiary` ;
+
+CREATE TABLE `TrainingDiary`.`athlete` (
+`aid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`name` VARCHAR( 100 ) NOT NULL ,
+`email` VARCHAR( 100 ) NOT NULL ,
+`fitnessdk` INT NULL 
+) ENGINE = MYISAM ;
+
+INSERT INTO `TrainingDiary`.`athlete` (
+`aid`, `name`, `email`, `fitnessdk`)
+VALUES (
+NULL, 'Test testesen', 'test@testmail.com', NULL)
+
+CREATE TABLE `TrainingDiary`.`weight` (
+`wid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'WeightID',
+`aid` INT NOT NULL COMMENT 'AthleteId',
+`date` DATE NOT NULL COMMENT 'Date',
+`weight` DECIMAL (3,1) NOT NULL COMMENT 'Weight/kg',
+`bodyfatpct` DECIMAL (2,1) NULL COMMENT 'Bodyfat/pct',
+`bodyfatkg` DECIMAL (3,1) NULL COMMENT 'Bodyfat/kg'
+) ENGINE = MYISAM ;
