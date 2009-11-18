82f1a94255cb198494a7add5039574ca54ede845
klb <klaus@klausberntsen.dk>
2009-11-18 22:46:52 +0100

    Added foreign key

--- mysql/create_table.sql
+++ mysql/create_table.sql
@@ -7,16 +7,26 @@ CREATE TABLE `TrainingDiary`.`athlete` (
 `fitnessdk` INT NULL 
 ) ENGINE = MYISAM ;
 
-INSERT INTO `TrainingDiary`.`athlete` (
-`aid`, `name`, `email`, `fitnessdk`)
-VALUES (
-NULL, 'Test testesen', 'test@testmail.com', NULL)
-
 CREATE TABLE `TrainingDiary`.`weight` (
 `wid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'WeightID',
-`aid` INT NOT NULL COMMENT 'AthleteId',
+FOREIGN KEY `aid` REFERENCES `TrainingDiary`.`athlete`.`aid` COMMENT 'AthleteId',
 `date` DATE NOT NULL COMMENT 'Date',
 `weight` DECIMAL (3,1) NOT NULL COMMENT 'Weight/kg',
 `bodyfatpct` DECIMAL (2,1) NULL COMMENT 'Bodyfat/pct',
 `bodyfatkg` DECIMAL (3,1) NULL COMMENT 'Bodyfat/kg'
 ) ENGINE = MYISAM ;
+
+
+/*
+INSERT INTO `TrainingDiary`.`athlete` (
+`aid`, `name`, `email`, `fitnessdk`)
+VALUES (
+NULL, 'Test testesen', 'test@testmail.com', NULL)
+
+INSERT INTO `trainingdiary`.`weight` (
+`wid` ,`aid` ,`date` ,`weight` ,`bodyfatpct` ,`bodyfatkg` )
+VALUES (
+NULL , '1', '2009-11-18', '75.9', '12', '9.1');
+
+*/
+
