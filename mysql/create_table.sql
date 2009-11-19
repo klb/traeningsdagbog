ce060086ae990c57e5084a929040af59976dc95a
unknown <kb@.axiell.local>
2009-11-19 15:51:48 +0100

    Changed tablestructure from MyISAM to InnoDB Cleaned up test-data

--- mysql/create_table.sql
+++ mysql/create_table.sql
@@ -1,3 +1,5 @@
+DROP DATABASE `trainingdiary` ;
+
 CREATE DATABASE `TrainingDiary` ;
 
 CREATE TABLE `TrainingDiary`.`athlete` (
@@ -5,26 +7,28 @@ CREATE TABLE `TrainingDiary`.`athlete` (
 `name` VARCHAR( 100 ) NOT NULL ,
 `email` VARCHAR( 100 ) NOT NULL ,
 `fitnessdk` INT NULL 
-) ENGINE = MYISAM ;
+) ENGINE = InnoDB ;
 
 CREATE TABLE `TrainingDiary`.`weight` (
 `wid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'WeightID',
-FOREIGN KEY `aid` REFERENCES `TrainingDiary`.`athlete`.`aid` COMMENT 'AthleteId',
-`date` DATE NOT NULL COMMENT 'Date',
+`aid` INT NOT NULL COMMENT 'AthleteID',
+INDEX (aid),
+FOREIGN KEY (`aid`) REFERENCES  `TrainingDiary`.`athlete` (`aid`) ON DELETE RESTRICT ON UPDATE CASCADE,
+`wdate` DATE NOT NULL COMMENT 'Date',
 `weight` DECIMAL (3,1) NOT NULL COMMENT 'Weight/kg',
 `bodyfatpct` DECIMAL (2,1) NULL COMMENT 'Bodyfat/pct',
 `bodyfatkg` DECIMAL (3,1) NULL COMMENT 'Bodyfat/kg'
-) ENGINE = MYISAM ;
+) ENGINE = InnoDB ;
 
 
 /*
 INSERT INTO `TrainingDiary`.`athlete` (
 `aid`, `name`, `email`, `fitnessdk`)
 VALUES (
-NULL, 'Test testesen', 'test@testmail.com', NULL)
+NULL, 'Test testesen', 'test@testmail.com', NULL);
 
 INSERT INTO `trainingdiary`.`weight` (
-`wid` ,`aid` ,`date` ,`weight` ,`bodyfatpct` ,`bodyfatkg` )
+`wid` ,`aid` ,`wdate` ,`weight` ,`bodyfatpct` ,`bodyfatkg` )
 VALUES (
 NULL , '1', '2009-11-18', '75.9', '12', '9.1');
 
